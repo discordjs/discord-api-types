a0b6cacf91b3667f0e51e2063e03654893e9d992
Advaith <advaithj1@gmail.com>
2020-09-26 16:41:40 -0700

    feat(v8): integration optional fields

--- v8/payloads/guild.ts
+++ v8/payloads/guild.ts
@@ -196,16 +196,16 @@ export interface APIGuildIntegration {
 	name: string;
 	type: string;
 	enabled: boolean;
-	syncing: boolean;
-	role_id: string;
+	syncing?: boolean;
+	role_id?: string;
 	enable_emoticons?: boolean;
-	expire_behavior: IntegrationExpireBehavior;
-	expire_grace_period: number;
+	expire_behavior?: IntegrationExpireBehavior;
+	expire_grace_period?: number;
 	user?: APIUser;
 	account: APIIntegrationAccount;
-	synced_at: string;
-	subscriber_count: number;
-	revoked: boolean;
+	synced_at?: string;
+	subscriber_count?: number;
+	revoked?: boolean;
 	application?: APIGuildIntegrationApplication;
 }
 
