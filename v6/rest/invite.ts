e2015c63e1a48a792653e1484e036598068a0b3b
Vlad Frangu <kingdgrizzle@gmail.com>
2020-10-04 22:35:00 +0300

    chore: release 0.6.0 (#23)

--- v6/rest/invite.ts
+++ v6/rest/invite.ts
@@ -8,6 +8,9 @@ export interface RESTGetAPIInviteQuery {
 	with_counts?: boolean;
 }
 
+/**
+ * @deprecated API v6 is deprecated and the types will not receive further updates, please update to v8.
+ */
 export type RESTGetAPIInviteResult = APIInvite;
 
 /**
