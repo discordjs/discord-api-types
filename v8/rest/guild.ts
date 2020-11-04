2ee557a59884b39634003edc72c88ceec7fcd661
Vlad Frangu <kingdgrizzle@gmail.com>
2020-11-04 11:38:45 +0200

    chore: remove RESTGetAPIGuildIntegrationQuery (per Discord) (#38)

--- v8/rest/guild.ts
+++ v8/rest/guild.ts
@@ -308,13 +308,6 @@ export type RESTGetAPIGuildVoiceRegionsResult = APIVoiceRegion[];
  */
 export type RESTGetAPIGuildInvitesResult = APIInvite[];
 
-/**
- * https://discord.com/developers/docs/resources/guild#get-guild-integrations
- */
-export interface RESTGetAPIGuildIntegrationsQuery {
-	include_applications?: boolean;
-}
-
 export type RESTGetAPIGuildIntegrationsResult = APIGuildIntegration[];
 
 /**
