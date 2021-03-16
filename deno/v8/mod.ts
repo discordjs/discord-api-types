85f95e448aaa89766d9ecca8680b66f7ace59f29
Vlad Frangu <kingdgrizzle@gmail.com>
2021-03-16 10:03:27 +0200

    chore: remove test message for precommit hook

--- deno/v8/mod.ts
+++ deno/v8/mod.ts
@@ -2,5 +2,3 @@ export * from '../common/mod.ts';
 export * from './gateway/mod.ts';
 export * from './payloads/mod.ts';
 export * from './rest/mod.ts';
-
-// OWO test
--- v8/index.ts
+++ v8/index.ts
@@ -2,5 +2,3 @@ export * from '../common/index';
 export * from './gateway/index';
 export * from './payloads/index';
 export * from './rest/index';
-
-// OWO test
