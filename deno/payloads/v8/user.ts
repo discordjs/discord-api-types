1774d4c4749d303f24bfb3c754cf79a4ca7ef699
Vlad Frangu <kingdgrizzle@gmail.com>
2021-04-10 19:16:57 +0300

    chore(UserFlags): un-document `SYSTEM` flag
    BREAKING CHANGE: This removes a flag that bots should not use, as Discord
    said this is an internal flag.
    
    Reference: https://github.com/discord/discord-api-docs/commit/9293f0d490ac6acf9d627e429e5a8131b303b528

--- deno/payloads/v8/user.ts
+++ deno/payloads/v8/user.ts
@@ -85,7 +85,6 @@ export enum UserFlags {
 	HypeSquadHouseBalance = 1 << 8,
 	EarlySupporter = 1 << 9,
 	TeamUser = 1 << 10,
-	System = 1 << 12,
 	BugHunterLevel2 = 1 << 14,
 	VerifiedBot = 1 << 16,
 	EarlyVerifiedBotDeveloper = 1 << 17,
