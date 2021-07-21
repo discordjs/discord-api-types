fb41b08a99c37598eb2d3fc73ba8295b6502b851
Vlad Frangu <kingdgrizzle@gmail.com>
2021-07-21 12:47:41 +0300

    fix(APIPingInteraction): there be no data here!

--- deno/payloads/v9/_interactions/ping.ts
+++ deno/payloads/v9/_interactions/ping.ts
@@ -1,4 +1,4 @@
 import type { APIBaseInteraction } from './base.ts';
 import type { InteractionType } from './responses.ts';
 
-export type APIPingInteraction = APIBaseInteraction<InteractionType.Ping, unknown>;
+export type APIPingInteraction = APIBaseInteraction<InteractionType.Ping, never>;
--- payloads/v9/_interactions/ping.ts
+++ payloads/v9/_interactions/ping.ts
@@ -1,4 +1,4 @@
 import type { APIBaseInteraction } from './base';
 import type { InteractionType } from './responses';
 
-export type APIPingInteraction = APIBaseInteraction<InteractionType.Ping, unknown>;
+export type APIPingInteraction = APIBaseInteraction<InteractionType.Ping, never>;
