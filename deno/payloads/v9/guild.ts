9f92cf7612964e924856a673158e5f10d1fc8e7c
suneettipirneni <suneettipirneni@icloud.com>
2021-11-09 18:38:46 -0500

    feat(APIGuildMember): add guild timeouts

--- deno/payloads/v9/guild.ts
+++ deno/payloads/v9/guild.ts
@@ -643,6 +643,10 @@ export interface APIGuildMember {
 	 * *If this field is not present, it can be assumed as `false`.*
 	 */
 	pending?: boolean;
+	/**
+	 * Timestamp of when the time out will be removed; until then, they cannot interact with the server
+	 */
+	communication_disabled_until?: string;
 }
 
 /**
