866a600b76663b6a438114c04f4a4cdcab00f96f
suneettipirneni <suneettipirneni@icloud.com>
2022-01-12 16:19:38 -0500

    types: add locale string types

--- rest/common.ts
+++ rest/common.ts
@@ -208,3 +208,38 @@ export const enum RESTJSONErrorCodes {
 
 	FailedToCreateStageNeededForStageEvent = 180002,
 }
+
+export const enum Locale {
+	EnglishUS = 'en-US',
+	EnglishGB = 'en-GB',
+	Bulgarian = 'bg',
+	ChineseCN = 'zh-CN',
+	ChineseTW = 'zh-TW',
+	Croatian = 'hr',
+	Czech = 'cs',
+	Danish = 'da',
+	Dutch = 'nl',
+	Finnish = 'fi',
+	French = 'fr',
+	German = 'de',
+	Greek = 'el',
+	Hindi = 'hi',
+	Hungarian = 'hu',
+	Italian = 'it',
+	Japanese = 'ja',
+	Korean = 'ko',
+	Lithuanian = 'lt',
+	Norwegian = 'no',
+	Polish = 'pl',
+	PortugueseBR = 'pt-BR',
+	Romanian = 'ro',
+	Russian = 'ru',
+	SpanishES = 'es-ES',
+	Swedish = 'sv-SE',
+	Thai = 'th',
+	Turkish = 'tr',
+	Ukrainian = 'uk',
+	Vietnamese = 'vi',
+}
+
+export type LocaleString = `${Locale}`;
