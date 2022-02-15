882e3ade2072dff29b18d021fc06a33d826dce0e
Vlad Frangu <kingdgrizzle@gmail.com>
2022-02-15 02:13:02 +0200

    chore: its not wrong

--- scripts/run-tsd.mjs
+++ scripts/run-tsd.mjs
@@ -1,10 +1,12 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call, @typescript-eslint/no-unsafe-member-access */
+// @ts-nocheck
+
 import tsd from 'tsd';
 import formatter from 'tsd/dist/lib/formatter.js';
 
 const diagnostics = await tsd.default({
 	cwd: process.cwd(),
 	typingsFile: './v10.d.ts',
-	// testFiles: ['./tests/**/*.test-d.ts'],
 });
 
 if (diagnostics.length > 0) {
