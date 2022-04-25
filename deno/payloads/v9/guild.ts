b974b00043911dfb648ba1bc3e0b2b1b702c83b0
Vitor <milagre.vitor@gmail.com>
2022-04-25 18:54:14 +0100

    refactor(GuildFeature): remove seven & three day thread archive

--- deno/payloads/v9/guild.ts
+++ deno/payloads/v9/guild.ts
@@ -537,14 +537,6 @@ export enum GuildFeature {
 	 */
 	RoleIcons = 'ROLE_ICONS',
 	/**
-	 * Guild has access to the seven day archive time for threads
-	 */
-	SevenDayThreadArchive = 'SEVEN_DAY_THREAD_ARCHIVE',
-	/**
-	 * Guild has access to the three day archive time for threads
-	 */
-	ThreeDayThreadArchive = 'THREE_DAY_THREAD_ARCHIVE',
-	/**
 	 * Guild has enabled ticketed events
 	 */
 	TicketedEventsEnabled = 'TICKETED_EVENTS_ENABLED',
