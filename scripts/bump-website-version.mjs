aaaaa5d5f6781d9ec5ab3a5d64f033a68d331fd5
Vlad Frangu <kingdgrizzle@gmail.com>
2022-05-30 22:08:38 +0300

    chore(docs): minify api-typedoc generated json

--- scripts/bump-website-version.mjs
+++ scripts/bump-website-version.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 import { execSync } from 'node:child_process';
-import { readFile } from 'node:fs/promises';
+import { readFile, writeFile } from 'node:fs/promises';
 
 const cwd = new URL('../website/', import.meta.url);
 const json = JSON.parse(await readFile(new URL('../package.json', import.meta.url), { encoding: 'utf8' }));
@@ -9,5 +9,11 @@ console.log(`⌛ Creating website version for ${json.version}`);
 
 execSync(`npm run docusaurus docs:version ${json.version}`, { cwd, encoding: 'utf8' });
 execSync(`npm run docusaurus api:version ${json.version}`, { cwd, encoding: 'utf8' });
+
+const bigJsonPath = new URL(`../website/versioned_docs/version-${json.version}/api-typedoc.json`, import.meta.url);
+
+const parsed = JSON.parse(await readFile(bigJsonPath, { encoding: 'utf8' }));
+
+await writeFile(bigJsonPath, JSON.stringify(parsed));
 
 console.log(`✅ Done! Website version created for ${json.version}`);
