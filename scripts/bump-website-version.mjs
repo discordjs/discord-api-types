1102744703e367620f3c849ea17e553d8ee84b7a
Vlad Frangu <kingdgrizzle@gmail.com>
2022-06-06 18:30:43 +0300

    chore: make website build again -.- Thanks Docusaurus!!

--- scripts/bump-website-version.mjs
+++ scripts/bump-website-version.mjs
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access */
 import { execSync } from 'node:child_process';
-import { readFile, writeFile } from 'node:fs/promises';
+import { readFile, rm, writeFile } from 'node:fs/promises';
 
 const cwd = new URL('../website/', import.meta.url);
 const json = JSON.parse(await readFile(new URL('../package.json', import.meta.url), { encoding: 'utf8' }));
@@ -16,4 +16,21 @@ const parsed = JSON.parse(await readFile(bigJsonPath, { encoding: 'utf8' }));
 
 await writeFile(bigJsonPath, JSON.stringify(parsed));
 
-console.log(`✅ Done! Website version created for ${json.version}`);
+console.log(`✅ Website version created for ${json.version}`);
+
+const versionsJsonPath = new URL('./versions.json', cwd);
+
+/** @type {string[]} */
+const allVersions = JSON.parse(await readFile(versionsJsonPath, 'utf8'));
+
+const versionToDelete = allVersions.pop();
+
+await writeFile(versionsJsonPath, JSON.stringify(allVersions, null, 2));
+
+await rm(new URL(`./versioned_docs/version-${versionToDelete}`, cwd), { force: true, recursive: true });
+await rm(new URL(`./versioned_sidebars/version-${versionToDelete}-sidebars.json`, cwd), {
+	force: true,
+	recursive: true,
+});
+
+console.log(`♻️ Removed old website version ${versionToDelete}`);
