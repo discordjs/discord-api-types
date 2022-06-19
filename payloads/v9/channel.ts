686f8493f351592fa8a0c454c8fbf29ea7148a79
Vitor <milagre.vitor@gmail.com>
2022-06-19 10:02:05 +0100

    refactor(APIMessage): `referenced_message` is a partial message

--- payloads/v9/channel.ts
+++ payloads/v9/channel.ts
@@ -464,7 +464,7 @@ export interface APIMessage {
 	 *
 	 * See https://discord.com/developers/docs/resources/channel#message-object
 	 */
-	referenced_message?: APIMessage | null;
+	referenced_message?: Partial<APIMessage> | null;
 	/**
 	 * Sent if the message is a response to an Interaction
 	 */
