a2d76fc7ca7b8e8bf3152cfce4a6a8a23610654d
MateoDeveloper <79017590+Mateo-tem@users.noreply.github.com>
2022-06-20 17:46:38 -0500

    chore(UserFlags): add `@unstable` to `Spammer` flag (#500)

--- payloads/v8/user.ts
+++ payloads/v8/user.ts
@@ -145,6 +145,8 @@ export enum UserFlags {
 	BotHTTPInteractions = 1 << 19,
 	/**
 	 * User has been identified as spammer
+	 *
+	 * @unstable This user flag is currently not documented by Discord but has a known value which we will try to keep up to date.
 	 */
 	Spammer = 1 << 20,
 }
