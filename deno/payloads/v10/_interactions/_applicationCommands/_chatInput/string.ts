2cade98ed0a0a074254fbc1580fc56d0e0b3dc9c
MateoDeveloper <79017590+Mateo-tem@users.noreply.github.com>
2022-07-02 05:54:15 -0500

    feat(APIApplicationCommandStringOption): add `min_length` and `max_length` (#513)

--- deno/payloads/v10/_interactions/_applicationCommands/_chatInput/string.ts
+++ deno/payloads/v10/_interactions/_applicationCommands/_chatInput/string.ts
@@ -5,8 +5,20 @@ import type {
 } from './base.ts';
 import type { APIApplicationCommandOptionChoice, ApplicationCommandOptionType } from './shared.ts';
 
+interface APIApplicationCommandStringOptionBase
+	extends APIApplicationCommandOptionBase<ApplicationCommandOptionType.String> {
+	/**
+	 * For option type `STRING`, the minimum allowed length (minimum of 0).
+	 */
+	min_length?: number;
+	/**
+	 * For option type `STRING`, the maximum allowed length (minimum of 1).
+	 */
+	max_length?: number;
+}
+
 export type APIApplicationCommandStringOption = APIApplicationCommandOptionWithAutocompleteOrChoicesWrapper<
-	APIApplicationCommandOptionBase<ApplicationCommandOptionType.String>,
+	APIApplicationCommandStringOptionBase,
 	APIApplicationCommandOptionChoice<string>
 >;
 
