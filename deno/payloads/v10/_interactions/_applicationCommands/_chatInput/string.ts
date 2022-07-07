d503c712520f67f2378a22d5149d1724af4d7a11
Parbez <imranbarbhuiya.fsd@gmail.com>
2022-07-07 22:01:20 +0530

    docs: mark the maximum allowed length for string option types (#514)

--- deno/payloads/v10/_interactions/_applicationCommands/_chatInput/string.ts
+++ deno/payloads/v10/_interactions/_applicationCommands/_chatInput/string.ts
@@ -8,11 +8,11 @@ import type { APIApplicationCommandOptionChoice, ApplicationCommandOptionType } 
 interface APIApplicationCommandStringOptionBase
 	extends APIApplicationCommandOptionBase<ApplicationCommandOptionType.String> {
 	/**
-	 * For option type `STRING`, the minimum allowed length (minimum of 0).
+	 * For option type `STRING`, the minimum allowed length (minimum of `0`, maximum of `6000`).
 	 */
 	min_length?: number;
 	/**
-	 * For option type `STRING`, the maximum allowed length (minimum of 1).
+	 * For option type `STRING`, the maximum allowed length (minimum of `1`, maximum of `6000`).
 	 */
 	max_length?: number;
 }
