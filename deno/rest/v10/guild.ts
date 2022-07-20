32bdd998a1b12c05a39d940eb6edc4ac8992ebef
advaith <advaithj1@gmail.com>
2022-07-20 16:56:02 -0700

    feat(RESTPutAPIGuildBanJSONBody): add `delete_message_seconds)

--- deno/rest/v10/guild.ts
+++ deno/rest/v10/guild.ts
@@ -573,8 +573,14 @@ export type RESTGetAPIGuildBanResult = APIBan;
 export type RESTPutAPIGuildBanJSONBody = AddUndefinedToPossiblyUndefinedPropertiesOfInterface<{
 	/**
 	 * Number of days to delete messages for (0-7)
+	 *
+	 * @deprecated use `delete_message_seconds` instead
 	 */
 	delete_message_days?: number;
+	/**
+	 * Number of seconds to delete messages for, between 0 and 604800 (7 days)
+	 */
+	delete_message_seconds?: number;
 }>;
 
 /**
