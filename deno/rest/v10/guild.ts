872ff11451ba04e109dd8708fccef726f820bc62
Vitor <milagre.vitor@gmail.com>
2022-08-01 20:24:37 +0100

    refactor: `channel_id` optional on modify user voice state

--- deno/rest/v10/guild.ts
+++ deno/rest/v10/guild.ts
@@ -855,7 +855,7 @@ export type RESTPatchAPIGuildVoiceStateCurrentMemberJSONBody = AddUndefinedToPos
 	/**
 	 * The id of the channel the user is currently in
 	 */
-	channel_id: Snowflake;
+	channel_id?: Snowflake;
 	/**
 	 * Toggles the user's suppress state
 	 */
