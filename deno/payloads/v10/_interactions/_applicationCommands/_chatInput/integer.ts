ebb4af4c8d17b8110e4b581c60e6c6465b26dd5a
MohamedAshraf <55748253+Op262@users.noreply.github.com>
2022-08-10 22:13:52 +0200

    feat(APIApplicationCommandBooleanOption): add min_length and max_length

--- deno/payloads/v10/_interactions/_applicationCommands/_chatInput/integer.ts
+++ deno/payloads/v10/_interactions/_applicationCommands/_chatInput/integer.ts
@@ -15,6 +15,14 @@ interface APIApplicationCommandIntegerOptionBase
 	 * If the option is an `INTEGER` or `NUMBER` type, the maximum value permitted.
 	 */
 	max_value?: number;
+	/**
+	 * For option type STRING, the minimum allowed length (minimum of 0, maximum of 6000)
+	 */
+	min_length?: number;
+	/**
+	 * 	For option type STRING, the maximum allowed length (minimum of 1, maximum of 6000)
+	 */
+	max_length?: number;
 }
 
 export type APIApplicationCommandIntegerOption = APIApplicationCommandOptionWithAutocompleteOrChoicesWrapper<
