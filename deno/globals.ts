c3aa579cf70a5732694a6a189968f7fa9ff16df4
Vitor <milagre.vitor@gmail.com>
2022-08-22 21:31:21 +0100

    refactor: use `SlashCommand`

--- deno/globals.ts
+++ deno/globals.ts
@@ -50,7 +50,7 @@ export const FormattingPatterns = {
 	 *
 	 * The `fullName` (possibly including `name`, `subcommandOrGroup` and `subcommand`) and `id` group properties is present on the `exec` result of this expression
 	 */
-	ApplicationCommand:
+	SlashCommand:
 		/<\/(?<fullName>(?<name>[\w-]{1,32})(?: (?<subcommandOrGroup>[\w-]{1,32}))?(?: (?<subcommand>[\w-]{1,32}))?):(?<id>\d{17,20})>/,
 	/**
 	 * Regular expression for matching a custom emoji, either static or animated
