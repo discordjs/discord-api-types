f20647390b46abae03cbc2c5263528feec30ceb3
Mateo <m.medinatech@gmail.com>
2022-10-04 21:08:27 -0500

    fix(StageInstancePrivacyLevel): revert removal of Public level

--- deno/payloads/v9/stageInstance.ts
+++ deno/payloads/v9/stageInstance.ts
@@ -42,7 +42,12 @@ export interface APIStageInstance {
  */
 export enum StageInstancePrivacyLevel {
 	/**
+	 * The stage instance is visible publicly, such as on stage discovery
+	 * @deprecated
+	 */
+	Public = 1,
+	/**
 	 * The stage instance is visible to only guild members
 	 */
-	GuildOnly = 2,
+	GuildOnly,
 }
