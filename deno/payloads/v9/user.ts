0858b7c70a4bc1431d28cc8f2df60461346f6a66
Almeida <almeidx@pm.me>
2022-11-02 07:06:54 +0000

    docs: remove ~ (#633)

--- deno/payloads/v9/user.ts
+++ deno/payloads/v9/user.ts
@@ -150,7 +150,7 @@ export enum UserFlags {
 	/**
 	 * User's account has been quarantined based on recent activity
 	 *
-	 * @unstable This user flag is currently not documented by Discord but has a known value which we will try to keep up to date.~
+	 * @unstable This user flag is currently not documented by Discord but has a known value which we will try to keep up to date.
 	 *
 	 * @privateRemarks
 	 *
