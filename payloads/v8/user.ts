6bf9a51a012b17e3ebfc7eae0545cd1d8a21a3ee
Eejit <76887639+Eejit43@users.noreply.github.com>
2022-11-23 21:04:32 -0500

    Link `Quarantined` to article

--- payloads/v8/user.ts
+++ payloads/v8/user.ts
@@ -150,11 +150,15 @@ export enum UserFlags {
 	 */
 	Spammer = 1 << 20,
 	/**
-	 * User's account has been quarantined based on recent activity
+	 * User's account has been [quarantined](https://support.discord.com/hc/en-us/articles/6461420677527) based on recent activity
 	 *
 	 * @unstable This user flag is currently not documented by Discord but has a known value which we will try to keep up to date.
+	 *
+	 * @privateRemarks
+	 *
+	 * This value would be 1 << 44, but bit shifting above 1 << 30 requires bigints
 	 */
-	Quarantined = Math.pow(2, 44),
+	Quarantined = 17592186044416,
 }
 
 /**
