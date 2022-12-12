699ede609f27ca8fec934f23ad78e0c1dc41cae8
Cynthia <cynthia@cynthia.dev>
2022-12-12 16:35:01 +0100

    fix(APIChannel): correctly type present properties based on channel type
    `default_thread_rate_limit_per_user` can be set on regular channels,
    despite the Discord client not showing this possibility, and newly
    created threads do abide by this configuration.
    
    `last_pin_timestamp` was not present on DM channels, despite them
    having this property.

--- tests/v10/channel.test-d.ts
+++ tests/v10/channel.test-d.ts
@@ -1,10 +1,24 @@
-import { expectType } from 'tsd';
-import type { ChannelType, APIPartialChannel, APIGroupDMChannel, APIDMChannel, APIGuildChannel } from '../../v10';
+import { expectType, expectError } from 'tsd';
+import type {
+	ChannelType,
+	APIPartialChannel,
+	APIGroupDMChannel,
+	APIDMChannel,
+	APIGuildChannel,
+	APITextChannel,
+	APIThreadChannel,
+	APIGuildVoiceChannel,
+	APIGuildStageVoiceChannel,
+} from '../../v10';
 
 declare const partialChannel: APIPartialChannel;
 declare const groupDMChannel: APIGroupDMChannel;
 declare const dmChannel: APIDMChannel;
 declare const guildChannel: APIGuildChannel<ChannelType>;
+declare const guildTextChannel: APITextChannel;
+declare const guildThreadChannel: APIThreadChannel;
+declare const guildVoiceChannel: APIGuildVoiceChannel;
+declare const guildVoiceStageChannel: APIGuildStageVoiceChannel;
 
 // Test channel names are properly typed
 // Always non-null present for non-DM channels, always null for DM channel
@@ -12,3 +26,15 @@ expectType<string | null | undefined>(partialChannel.name);
 expectType<string | null>(groupDMChannel.name);
 expectType<null>(dmChannel.name);
 expectType<string>(guildChannel.name);
+
+// Test last pin timestamp
+expectType<string | null | undefined>(dmChannel.last_pin_timestamp);
+expectType<string | null | undefined>(groupDMChannel.last_pin_timestamp);
+expectType<string | null | undefined>(guildTextChannel.last_pin_timestamp);
+expectType<string | null | undefined>(guildThreadChannel.last_pin_timestamp);
+expectError(guildVoiceChannel.last_pin_timestamp);
+expectError(guildVoiceStageChannel.last_pin_timestamp);
+
+// Test rate limit types
+expectType<number | undefined>(guildTextChannel.default_thread_rate_limit_per_user);
+expectError(guildVoiceChannel.default_thread_rate_limit_per_user);
