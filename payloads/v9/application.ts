4c334f6eb83f6e01a79963011ce9c376b888e901
Almeida <almeidx@pm.me>
2022-12-22 18:10:03 +0000

    docs(ApplicationFlags): add descriptions to each flag (#639)

--- payloads/v9/application.ts
+++ payloads/v9/application.ts
@@ -122,19 +122,64 @@ export interface APIApplicationInstallParams {
  * https://discord.com/developers/docs/resources/application#application-object-application-flags
  */
 export enum ApplicationFlags {
+	/**
+	 * @unstable
+	 */
 	EmbeddedReleased = 1 << 1,
+	/**
+	 * @unstable
+	 */
 	ManagedEmoji = 1 << 2,
+	/**
+	 * @unstable
+	 */
 	GroupDMCreate = 1 << 4,
+	/**
+	 * @unstable
+	 */
 	RPCHasConnected = 1 << 11,
+	/**
+	 * Intent required for bots in 100 or more servers to receive `presence_update` events
+	 */
 	GatewayPresence = 1 << 12,
+	/**
+	 * Intent required for bots in under 100 servers to receive `presence_update` events, found in Bot Settings
+	 */
 	GatewayPresenceLimited = 1 << 13,
+	/**
+	 * Intent required for bots in 100 or more servers to receive member-related events like `guild_member_add`.
+	 * See list of member-related events [under `GUILD_MEMBERS`](https://discord.com/developers/docs/topics/gateway#list-of-intents)
+	 */
 	GatewayGuildMembers = 1 << 14,
+	/**
+	 * Intent required for bots in under 100 servers to receive member-related events like `guild_member_add`, found in Bot Settings.
+	 * See list of member-related events [under `GUILD_MEMBERS`](https://discord.com/developers/docs/topics/gateway#list-of-intents)
+	 */
 	GatewayGuildMembersLimited = 1 << 15,
+	/**
+	 * Indicates unusual growth of an app that prevents verification
+	 */
 	VerificationPendingGuildLimit = 1 << 16,
+	/**
+	 * Indicates if an app is embedded within the Discord client (currently unavailable publicly)
+	 */
 	Embedded = 1 << 17,
+	/**
+	 * Intent required for bots in 100 or more servers to receive [message content](https://support-dev.discord.com/hc/en-us/articles/4404772028055)
+	 */
 	GatewayMessageContent = 1 << 18,
+	/**
+	 * Intent required for bots in under 100 servers to receive [message content](https://support-dev.discord.com/hc/en-us/articles/4404772028055),
+	 * found in Bot Settings
+	 */
 	GatewayMessageContentLimited = 1 << 19,
+	/**
+	 * @unstable
+	 */
 	EmbeddedFirstParty = 1 << 20,
+	/**
+	 * Indicates if an app has registered global [application commands](https://discord.com/developers/docs/interactions/application-commands)
+	 */
 	ApplicationCommandBadge = 1 << 23,
 }
 
