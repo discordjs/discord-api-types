d0763b1e9a431b1e6e24908843f680fd9ddaf928
Jiralite <33201955+Jiralite@users.noreply.github.com>
2023-04-29 21:20:58 +0100

    fix: omit the guild id

--- gateway/v9.ts
+++ gateway/v9.ts
@@ -975,6 +975,11 @@ export type GatewayGuildMembersChunkDispatch = DataPayload<
 >;
 
 /**
+ * https://discord.com/developers/docs/topics/gateway-events#update-presence
+ */
+export type GatewayGuildMembersChunkPresence = Omit<RawGatewayPresenceUpdate, 'guild_id'>;
+
+/**
  * https://discord.com/developers/docs/topics/gateway-events#guild-members-chunk
  */
 export interface GatewayGuildMembersChunkDispatchData {
@@ -1005,7 +1010,7 @@ export interface GatewayGuildMembersChunkDispatchData {
 	 *
 	 * See https://discord.com/developers/docs/topics/gateway-events#update-presence
 	 */
-	presences?: RawGatewayPresenceUpdate[];
+	presences?: GatewayGuildMembersChunkPresence[];
 	/**
 	 * The nonce used in the Guild Members Request
 	 *
