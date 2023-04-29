5bb8966ddec31ce214bac908a99f3b076b11fbe0
almeidx <almeidx@pm.me>
2023-04-29 23:33:08 +0100

    fix: allow sending empty choices with autocomplete: true

--- deno/payloads/v10/_interactions/_applicationCommands/_chatInput/base.ts
+++ deno/payloads/v10/_interactions/_applicationCommands/_chatInput/base.ts
@@ -22,6 +22,7 @@ export type APIApplicationCommandOptionWithAutocompleteOrChoicesWrapper<
 > =
 	| (Base & {
 			autocomplete: true;
+			choices?: never[];
 	  })
 	| (Base & {
 			autocomplete?: false;
