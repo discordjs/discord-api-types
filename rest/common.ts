28ed3701e6105d0d15fb988194c13079a27e4369
Almeida <almeidx@pm.me>
2023-10-28 10:24:36 +0100

    feat(RESTJSONErrorCodes): add `40074` and `50057` (#844)

--- rest/common.ts
+++ rest/common.ts
@@ -154,6 +154,8 @@ export enum RESTJSONErrorCodes {
 	ThereAreNoTagsAvailableThatCanBeSetByNonModerators = 40_066,
 	TagRequiredToCreateAForumPostInThisChannel,
 
+	AnEntitlementHasAlreadyBeenGrantedForThisResource = 40_074,
+
 	MissingAccess = 50_001,
 	InvalidAccountType,
 	CannotExecuteActionOnDMChannel,
@@ -197,6 +199,8 @@ export enum RESTJSONErrorCodes {
 
 	CannotSelfRedeemThisGift = 50_054,
 	InvalidGuild,
+
+	InvalidSKU = 50_057,
 
 	InvalidRequestOrigin = 50_067,
 	InvalidMessageType,
