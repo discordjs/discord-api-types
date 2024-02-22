d6f33ee96dd25ab011f45975ae7f97bf7ec8360e
Mateo <m.medinatech@gmail.com>
2024-02-21 19:04:21 -0500

    feat: add type query param for get reactions endpoint

--- rest/v9/channel.ts
+++ rest/v9/channel.ts
@@ -382,6 +382,10 @@ export type RESTDeleteAPIChannelMessageUserReactionResult = never;
  */
 export interface RESTGetAPIChannelMessageReactionUsersQuery {
 	/**
+	 * The reaction type (0 for normal and 1 for super)
+	 */
+	type: ReactionType;
+	/**
 	 * Get users after this user ID
 	 */
 	after?: Snowflake;
@@ -391,6 +395,14 @@ export interface RESTGetAPIChannelMessageReactionUsersQuery {
 	 * @default 25
 	 */
 	limit?: number;
+}
+
+/**
+ * https://discord.com/developers/docs/resources/channel#get-reactions-reaction-types
+ */
+export enum ReactionType {
+	Normal,
+	Super,
 }
 
 /**
