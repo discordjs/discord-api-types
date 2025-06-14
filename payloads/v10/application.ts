758dfc8956966957f445a08dc07614de7de6ec55
Danial Raza <danialrazafb@gmail.com>
2025-06-14 16:26:10 +0200

    feat(APIApplication): add `approximate_user_authorization_count`

--- payloads/v10/application.ts
+++ payloads/v10/application.ts
@@ -111,9 +111,13 @@ export interface APIApplication {
 	 */
 	approximate_guild_count?: number;
 	/**
-	 * Approximate count of users that have installed the app
+	 * Approximate count of users that have installed the app (authorized with `application.commands` as a scope)
 	 */
 	approximate_user_install_count?: number;
+	/**
+	 * Approximate count of users that have OAuth2 authorizations for the app
+	 */
+	approximate_user_authorization_count?: number;
 	/**
 	 * Array of redirect URIs for the application
 	 */
