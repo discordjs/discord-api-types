e208da39dc77b4f606178fe6c14a68a5913299e5
Jiralite <33201955+Jiralite@users.noreply.github.com>
2025-08-07 08:06:10 +0100

    fix: modal submit type

--- payloads/v10/_interactions/modalSubmit.ts
+++ payloads/v10/_interactions/modalSubmit.ts
@@ -1,4 +1,4 @@
-import type { APIBaseComponent, APILabelComponent } from '../channel';
+import type { APIBaseComponent, APIStringSelectComponentInModal, APITextInputComponentInModal } from '../channel';
 import type {
 	APIBaseInteraction,
 	APIDMInteractionWrapper,
@@ -18,7 +18,7 @@ export interface ModalSubmitActionRowComponent extends APIBaseComponent<Componen
 }
 
 export interface ModalSubmitLabelComponent extends APIBaseComponent<ComponentType.Label> {
-	component: APILabelComponent;
+	component: APIStringSelectComponentInModal | APITextInputComponentInModal;
 }
 
 /**
