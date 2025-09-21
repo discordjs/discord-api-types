41a2c3fca58b389becf8d09af5b309910e33eaad
Nabil-249 <NabilAhmedIbrahim@outlook.com>
2025-09-21 21:08:02 +0800

    fix(Channel): fix inference for applied_tags
    only show applied_tags field in APIGuildForumChannel and APIGuildMediaChannel

--- deno/payloads/v9/channel.ts
+++ deno/payloads/v9/channel.ts
@@ -271,10 +271,6 @@ export interface APIThreadChannel<Type extends ThreadChannelType = ThreadChannel
 	 * Similar to `message_count` on message creation, but won't decrement when a message is deleted
 	 */
 	total_message_sent?: number;
-	/**
-	 * The IDs of the set of tags that have been applied to a thread in a thread-only channel
-	 */
-	applied_tags: Snowflake[];
   /**
 	 * ID of the parent channel for the thread
 	 */
@@ -401,6 +397,10 @@ export interface APIThreadOnlyChannel<T extends ChannelType.GuildForum | Channel
 	 * The default sort order type used to order posts in a thread-only channel
 	 */
 	default_sort_order: SortOrderType | null;
+  /**
+	 * The IDs of the set of tags that have been applied to a thread in a thread-only channel
+	 */
+	applied_tags: Snowflake[];
 }
 
 export interface APIGuildForumChannel extends APIThreadOnlyChannel<ChannelType.GuildForum> {
