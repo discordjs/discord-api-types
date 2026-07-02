9833871fba4b2e4f3ba19db55088e54e2c0feca1
anisha imam <137700126+imnaiyar@users.noreply.github.com>
2026-07-02 11:42:09 +0530

    chore: remove comments from attachment option

--- payloads/v9/_interactions/_applicationCommands/_chatInput/attachment.ts
+++ payloads/v9/_interactions/_applicationCommands/_chatInput/attachment.ts
@@ -5,12 +5,6 @@ import type { ApplicationCommandOptionType } from './shared';
 export type FileUploadType = 'audio' | 'image' | 'video' | `.${string}`;
 
 export interface APIApplicationCommandAttachmentOption extends APIApplicationCommandOptionBase<ApplicationCommandOptionType.Attachment> {
-	/**
-	 * Allowed file types that can be uploaded; max 10
-	 *
-	 * If only dot-prefixed extensions is specified, due to mobile limitation, you must include `.jpg` for image uploads,
-	 * and both `.mp4` and `.mov` for video uploads
-	 */
 	file_types?: FileUploadType[];
 }
 
